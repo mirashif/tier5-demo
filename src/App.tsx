5f06386549a6d7cfd64891f230b43a7cc44bfd2b
Mir Ashif <mirashif01@gmail.com>
2022-08-16 00:19:19 +0600

    added react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,12 @@
-import { Container, Flex, useBreakpointValue } from "@chakra-ui/react";
-import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
 
-import {
-  AddPost,
-  Header,
-  Posts,
-  ShowMenu,
-  Sidebar,
-  SidebarVariant,
-} from "~/components";
-
-type Breakpoint = { sidebar: SidebarVariant; showMenu: ShowMenu };
-
-const base: Breakpoint = {
-  sidebar: "drawer",
-  showMenu: true,
-};
-const lg: Breakpoint = { sidebar: "sidebar", showMenu: false };
+import { Home, Facebook } from "~/pages";
 
 export function App() {
-  const [isSidebarOpen, setSidebarOpen] = useState(false);
-  const variants = useBreakpointValue<Breakpoint>({
-    base,
-    lg,
-  });
-
-  const toggleSidebar = () => setSidebarOpen(!isSidebarOpen);
-
   return (
-    <Flex direction="column" align="center">
-      <Header
-        showMenuButton={variants?.showMenu as ShowMenu}
-        onShowSidebar={toggleSidebar}
-      />
-      <Flex
-        gap={4}
-        px="4"
-        maxW="5xl"
-        align="start"
-        justify="space-between"
-        zIndex="base"
-      >
-        <Container as="main" w="full" px="0">
-          <AddPost />
-          <Posts />
-        </Container>
-        <Sidebar
-          variant={variants?.sidebar as SidebarVariant}
-          isOpen={isSidebarOpen}
-          onClose={toggleSidebar}
-        />
-      </Flex>
-    </Flex>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/facebook" element={<Facebook />} />
+    </Routes>
   );
 }
---
+++ src/pages/Facebook.tsx
@@ -0,0 +1,56 @@
+import { Container, Flex, useBreakpointValue } from "@chakra-ui/react";
+import { useState } from "react";
+
+import {
+  AddPost,
+  Header,
+  Posts,
+  ShowMenu,
+  Sidebar,
+  SidebarVariant,
+} from "~/components";
+
+type Breakpoint = { sidebar: SidebarVariant; showMenu: ShowMenu };
+
+const base: Breakpoint = {
+  sidebar: "drawer",
+  showMenu: true,
+};
+const lg: Breakpoint = { sidebar: "sidebar", showMenu: false };
+
+export function Facebook() {
+  const [isSidebarOpen, setSidebarOpen] = useState(false);
+  const variants = useBreakpointValue<Breakpoint>({
+    base,
+    lg,
+  });
+
+  const toggleSidebar = () => setSidebarOpen(!isSidebarOpen);
+
+  return (
+    <Flex direction="column" align="center">
+      <Header
+        showMenuButton={variants?.showMenu as ShowMenu}
+        onShowSidebar={toggleSidebar}
+      />
+      <Flex
+        gap={4}
+        px="4"
+        maxW="5xl"
+        align="start"
+        justify="space-between"
+        zIndex="base"
+      >
+        <Container as="main" w="full" px="0">
+          <AddPost />
+          <Posts />
+        </Container>
+        <Sidebar
+          variant={variants?.sidebar as SidebarVariant}
+          isOpen={isSidebarOpen}
+          onClose={toggleSidebar}
+        />
+      </Flex>
+    </Flex>
+  );
+}
