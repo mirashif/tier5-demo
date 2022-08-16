23d6d68b8b9622e6a9956562e7c58515c9bcbe5e
Mir Ashif <mirashif01@gmail.com>
2022-08-17 02:44:59 +0600

    added stories component

---
+++ src/components/instagram/Stories.tsx
@@ -0,0 +1,43 @@
+import { Flex, Circle, Image, Text } from "@chakra-ui/react";
+
+import { useInstagramStore } from "~/store";
+
+export const Stories = () => {
+  const stories = useInstagramStore((state) => state.stories);
+  return (
+    <Flex
+      as="section"
+      aria-label="Stories"
+      p="4"
+      mt="5"
+      gap="4"
+      border="1px"
+      borderColor="gray.200"
+      borderRadius="lg"
+      overflowX="auto"
+    >
+      {stories.map(({ id, user }) => (
+        <Flex
+          key={id}
+          gap="2"
+          direction="column"
+          align="center"
+          justify="center"
+          maxW="66px"
+        >
+          <Circle
+            size="64px"
+            bgGradient="linear-gradient(45deg, #f09433 0%,#e6683c 25%,#dc2743 50%,#cc2366 75%,#bc1888 100%)"
+          >
+            <Circle p="2px" bg="white">
+              <Image src={user.avatar} boxSize="56px" borderRadius="full" />
+            </Circle>
+          </Circle>
+          <Text fontSize="12px" maxW="66px" noOfLines={1}>
+            {user.username}
+          </Text>
+        </Flex>
+      ))}
+    </Flex>
+  );
+};
--- src/pages/Instagram.tsx
+++ src/pages/Instagram.tsx
@@ -1,10 +1,8 @@
-import { Circle, Container, Flex, Image, Text } from "@chakra-ui/react";
+import { Container, Flex } from "@chakra-ui/react";
 
-import { Header, Posts } from "~/components/instagram";
-import { useInstagramStore } from "~/store";
+import { Header, Posts, Stories } from "~/components/instagram";
 
 export function Instagram() {
-  const stories = useInstagramStore((state) => state.stories);
   return (
     <Flex direction="column" align="center">
       <Header />
@@ -23,45 +21,7 @@ export function Instagram() {
           px="0"
           mx="0"
         >
-          {/* stories */}
-          <Flex
-            as="section"
-            aria-label="Stories"
-            p="4"
-            mt="5"
-            gap="4"
-            border="1px"
-            borderColor="gray.200"
-            borderRadius="lg"
-            overflowX="auto"
-          >
-            {stories.map(({ id, user }) => (
-              <Flex
-                key={id}
-                gap="2"
-                direction="column"
-                align="center"
-                justify="center"
-                maxW="66px"
-              >
-                <Circle
-                  size="64px"
-                  bgGradient="linear-gradient(45deg, #f09433 0%,#e6683c 25%,#dc2743 50%,#cc2366 75%,#bc1888 100%)"
-                >
-                  <Circle p="2px" bg="white">
-                    <Image
-                      src={user.avatar}
-                      boxSize="56px"
-                      borderRadius="full"
-                    />
-                  </Circle>
-                </Circle>
-                <Text fontSize="12px" maxW="66px" noOfLines={1}>
-                  {user.username}
-                </Text>
-              </Flex>
-            ))}
-          </Flex>
+          <Stories />
           <Posts />
         </Container>
       </Flex>
